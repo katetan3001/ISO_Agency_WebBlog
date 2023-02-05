5339e46b8ae7e0af71260d19679ce760fd3f3ffb
katedo00 <meodenmeohen@gmail.com>
2023-02-05 10:48:11 +0700

    rm-import-script

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import ImportScript from "./ImportScript"
 import "./App.css";
 
 export default function App () {
-  ImportScript('assets/js/swiper.js');
-  ImportScript('assets/js/countto.js');
-  ImportScript('assets/js/shortcodes.js');
-  ImportScript('assets/js/plugin.js');
+  // ImportScript('assets/js/swiper.js');
+  // ImportScript('assets/js/countto.js');
+  // ImportScript('assets/js/shortcodes.js');
+  // ImportScript('assets/js/plugin.js');
     return (
       <React.Fragment>
         <Loader />
